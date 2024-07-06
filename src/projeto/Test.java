c6cd5bc003c94a4fdff909295e34921ea04cd983
Ionailton Silva <166658906+IonailtonSilva@users.noreply.github.com>
2024-07-06 12:16:43 -0300

    Initial commit

---
+++ src/projeto/Test.java
@@ -0,0 +1,57 @@
+package projeto;
+
+import projeto.facade.Facade;
+import projeto.singleton.SingletonEager;
+import projeto.singleton.SingletonLazy;
+import projeto.singleton.SingletonLazyHolder;
+import projeto.strategy.Comportamento;
+import projeto.strategy.ComportamentoAgressivo;
+import projeto.strategy.ComportamentoDefensivo;
+import projeto.strategy.ComportamentoNormal;
+import projeto.strategy.Robo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		// Singleton
+		
+		SingletonLazy lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		
+		SingletonEager eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		
+		SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
+		lazyHolder = SingletonLazyHolder.getInstancia();
+		System.out.println(lazyHolder);
+		
+		// Strategy
+		
+		Comportamento defensivo = new ComportamentoDefensivo();
+		Comportamento normal = new ComportamentoNormal();
+		Comportamento agressivo = new ComportamentoAgressivo();
+		
+		Robo robo = new Robo();
+		robo.setComportamento(normal);
+		robo.mover();
+		robo.mover();
+		robo.setComportamento(defensivo);
+		robo.mover();
+		robo.setComportamento(agressivo);
+		robo.mover();
+		robo.mover();
+		robo.mover();
+		
+		// Facade
+		
+		Facade facade = new Facade();
+		facade.migrarCliente("Venilton", "14801788");
+	}
+
+}
